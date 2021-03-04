df142e2d104abf0fa0cd6bfebcb1f1e126c4ab5a
jaime.ponsg@estudiante.uam.es <jaime.ponsg@estudiante.uam.es>
2021-03-04 23:15:01 +0100

    socket.h.c y pythonthest

---
+++ socket.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <strings.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <syslog.h>
+
+
+
+
+
+
+int server_ini(){
+
+    int sockvalue;
+    struct sockaddr_in Dir;
+    
+    if((sockvalue = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        exit(EXIT_FAILURE);
+    }
+
+    Dir.sin_family = AF_INET;
+    //Dir.sin_port = htons(NFC_SERVER_PORT);
+    Dir.sin_addr.s_addr = htonl(INADDR_ANY);
+    bzero((void*)&(Dir.sin_zero), 8);
+
+
+    if(bind(sockvalue, (struct sockaddr *)&Dir, sizeof(Dir) < 0)){
+        exit(EXIT_FAILURE);
+    }
+
+    return sockvalue;
+}
+
+void accept_connection (int sockval){
+    int desc;
+    struct sockaddr Link;
+    socklen_t len;
+    
+    len = sizeof(Link);
+
+    if(desc = accept(sockval,&Link,&len) <0){
+        exit(EXIT_FAILURE);
+    }
+
+    launch_service(desc);
+    //wait();
+    return;
+}
+
+void launch_service(int connval){
+    int pid;
+    long type, aux;
+    pid = fork();
+    if(pid < 0) exit (EXIT_FAILURE);
+    if (pid == 0) return;
+
+    syslog (LOG_INFO, "New access");
+    recv(connval, &aux, sizeof(long), 0);
+    type = ntohl(aux);
+
+
+    //database_access(connval, type, NULL);
+    close(connval);
+    syslog (LOG_INFO, "Exiting service");
+    exit(EXIT_SUCCESS);
+}
---
+++ socket.h
@@ -0,0 +1,51 @@
+#ifndef _socket
+
+#define _socket
+
+#include "stdio.h"
+//Funcion de encapsulamiento (utlizando sys/socket.h)
+int socket(int family, int type, int protocol);
+//Funcion de encapsulamiento (utlizando sys/socket.h)
+int bind(int socket, struct sockaddr * localaddr, int addrlen);
+//Funcion de encapsulamiento (utlizando sys/socket.h)
+int listen(int socket, int queue_size);
+//Funcion de encapsulamiento (utlizando sys/socket.h)
+//int accept( int socket, struct sockaddr * foreignaddr, int addrlen);
+//Funcion de encapsulamiento (utlizando sys/socket.h)
+int connect(int sockfd, struct sockaddr * foreignaddrr, int addrlen);
+//Funcion de encapsulamiento (utlizando sys/socket.h)
+int close(int socket);
+//Funcion de encapsulamiento (utlizando sys/socket.h)
+int send(int socket, void *buf, size_t len, int flags);
+//Funcion de encapsulamiento (utlizando sys/socket.h)
+int read(int fd, void *buf, size_t count);
+/**
+ * Funcion: httpParser : Ajusta una cadena de texto para ser procesada por el servidor
+ * Argumentos:
+ * 
+ * Retorno:
+**/
+
+/**
+ * Funcion: server_ini : Inicializa un servidor asignandole socket y comenzando el proceso de recepcion
+ * Argumentos:
+ * 
+ * Retorno:
+ **/
+int server_ini();
+/**
+ * Funcion: accept_connection : Acepta una peticion de conexion al server
+ * Argumentos:
+ * sockval: Identificador de un socket
+ * Retorno:
+ **/
+void accept_connection (int sockval);
+/**
+ * Funcion: launch_service: Una vez que se acepta una conexion, se lanza para recibir el mensaje en el servidor
+ * Argumentos:
+ * connval: Retorno de la funcion accept (accept_connection)
+ * Retorno:
+ **/
+void launch_service(int connval);
+
+#include "socket.c"
